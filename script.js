009e2b4294c5295fdf53cf1b1746e6c26ff246ad
keeechi <rs32329@yahoo.co.jp>
2025-07-06 14:52:01 +0900

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const sleepFaces = [
+    {
+        pokemon: "フシギダネ",
+        face: "★☆☆☆☆",
+        conditions: [
+            { field: "ワカクサ本島", minRank: 2 },
+            { field: "ラピスラズリ湖畔", minRank: 1 }
+        ]
+    },
+    {
+        pokemon: "フシギダネ",
+        face: "★★☆☆☆",
+        conditions: [
+            { field: "ワカクサ本島", minRank: 6 },
+            { field: "ラピスラズリ湖畔", minRank: 1 }
+        ]
+    }
+];
